b4f05fdede18534c1355234e6763ab1a2cb8791f
binhnguyen <leechiupin@gmail.com>
2023-10-14 13:10:49 +0900

    Thêm tính năng tìm kiếm, lọc person theo tên

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,7 +15,7 @@ const xetLoaiDoiTuong = () => {
     let customerTab2 = getEle("#customerTab2");
     let customerTab3 = getEle("#customerTab3");
 
-    if ( loaiDoiTuong === "Student" ) {
+    if (loaiDoiTuong === "Student") {
         employeeTab1.classList.remove("d-block");
         employeeTab1.classList.add("d-none");
         employeeTab2.classList.remove("d-block");
@@ -34,7 +34,7 @@ const xetLoaiDoiTuong = () => {
         studentTab3.classList.remove("d-none");
         studentTab3.classList.add("d-block");
     }
-    else if ( loaiDoiTuong === "Employee" ) {
+    else if (loaiDoiTuong === "Employee") {
         studentTab1.classList.remove("d-block");
         studentTab1.classList.add("d-none");
         studentTab2.classList.remove("d-block");
@@ -53,7 +53,7 @@ const xetLoaiDoiTuong = () => {
         employeeTab2.classList.remove("d-none");
         employeeTab2.classList.add("d-block");
     }
-    else if ( loaiDoiTuong === "Customer" ) {
+    else if (loaiDoiTuong === "Customer") {
         studentTab1.classList.remove("d-block");
         studentTab1.classList.add("d-none");
         studentTab2.classList.remove("d-block");
@@ -100,22 +100,22 @@ const xetLoaiDoiTuong = () => {
  * Chú ý:
  */
 const fetchPerson = () => {
-    
+
     getPersonList()
-    // thành công thì in ra sp
-    .then(function (res) {
-        renderTable(res.data);
-        //offLoading();
-    })
-    // thất bại thì báo lỗi, hoặc in ra thông báo tuỳ mình
-    .catch(function (err) {
-    //   offLoading();
-      console.log("err", err);
-    })
-    // luôn luôn chạy dù thành công, thất bại
-    .finally(() =>{
-        console.log("Xong");
-    });
+        // thành công thì in ra sp
+        .then(function (res) {
+            renderTable(res.data);
+            //offLoading();
+        })
+        // thất bại thì báo lỗi, hoặc in ra thông báo tuỳ mình
+        .catch(function (err) {
+            //   offLoading();
+            console.log("err", err);
+        })
+        // luôn luôn chạy dù thành công, thất bại
+        .finally(() => {
+            console.log("Xong");
+        });
 }
 fetchPerson();
 
@@ -126,7 +126,7 @@ fetchPerson();
  * Tham số: không
  * Chú ý:
  */
-const renderTable = (list) =>{
+const renderTable = (list) => {
     let htmlContent = "";
     list.forEach((value) => {
         htmlContent += `
@@ -159,7 +159,7 @@ const getInfo = () => {
     let person = {};
 
     elements.forEach((ele, index) => {
-        console.log(ele.value, ele.name, index)
+        // console.log(ele.value, ele.name, index)
         //destructuring
         const { value, name } = ele;
         person[name] = value;
@@ -167,16 +167,16 @@ const getInfo = () => {
     });
 
     // Destructuring (bóc tách phần tử)
-    const {id, namePerson, address, email, type, diemToan, diemLy, diemHoa, day, salary, congTy, hoaDon, danhGia} = person;
+    const { id, namePerson, address, email, type, diemToan, diemLy, diemHoa, day, salary, congTy, hoaDon, danhGia } = person;
 
-    if ( person.type === "Student") {
-        return new Student( id, namePerson, address, email, type, diemToan, diemLy, diemHoa );
+    if (person.type === "Student") {
+        return new Student(id, namePerson, address, email, type, diemToan, diemLy, diemHoa);
     }
-    else if ( person.type === "Employee" ) {
-        return new Employee( id, namePerson, address, email, type, day, salary );
+    else if (person.type === "Employee") {
+        return new Employee(id, namePerson, address, email, type, day, salary);
     }
-    else if ( person.type === "Customer" ) {
-        return new Customer( id, namePerson, address, email, type, congTy, hoaDon, danhGia );
+    else if (person.type === "Customer") {
+        return new Customer(id, namePerson, address, email, type, congTy, hoaDon, danhGia);
     }
     else {
         // do nothing
@@ -190,27 +190,27 @@ const getInfo = () => {
  * Tham số: không
  * Chú ý: thêm hàm editFood vào đối tượng window, khi mình ấn edit thì nó sẽ đi tìm trong window có hàm editFood hay ko
  */
-const addPerson = () =>{
+const addPerson = () => {
     // lấy thông tin món ăn từ user nhập lên form
     const person = getInfo();
     // console.log('person: ', person);
 
     addPersonList(person)
-    .then((res) => {
-        console.log("res.data", res.data);
-        // call API lấy lại danh sách người dùng mới sau khi thêm thành công
-        fetchPerson();
-        // Đóng modal sau khi add
-        getEle("#btnDong").click();
+        .then((res) => {
+            console.log("res.data", res.data);
+            // call API lấy lại danh sách người dùng mới sau khi thêm thành công
+            fetchPerson();
+            // Đóng modal sau khi add
+            getEle("#btnDong").click();
 
-    })
-    .catch((err) => {
-        console.log("err: ", err);
-    })
-    // luôn luôn chạy dù thành công, thất bại
-    .finally(() =>{
-        console.log("Xong");
-    });
+        })
+        .catch((err) => {
+            console.log("err: ", err);
+        })
+        // luôn luôn chạy dù thành công, thất bại
+        .finally(() => {
+            console.log("Xong");
+        });
 }
 
 
@@ -225,18 +225,18 @@ const addPerson = () =>{
  */
 window.deletePerson = (id) => {
     delPersonList(id)
-    .then((res) => {
-        console.log("res.data", res.data);
-        // call API lấy lại danh sách món ăn mới sau khi xoá thành công
-        fetchPerson();
-    })
-    .catch((err) => {
-        console.log("err: ", err);
-    })
-    // luôn luôn chạy dù thành công, thất bại
-    .finally(() =>{
-        console.log("Xong");
-    });
+        .then((res) => {
+            console.log("res.data", res.data);
+            // call API lấy lại danh sách món ăn mới sau khi xoá thành công
+            fetchPerson();
+        })
+        .catch((err) => {
+            console.log("err: ", err);
+        })
+        // luôn luôn chạy dù thành công, thất bại
+        .finally(() => {
+            console.log("Xong");
+        });
 }
 
 
@@ -257,45 +257,45 @@ window.editPerson = (id) => {
     getEle('#btnCapNhat').style.display = 'inline-block'
 
     editPersonByID(id)
-    .then((res) => {
-        console.log("res.data", res.data);
-        let person = res.data
+        .then((res) => {
+            console.log("res.data", res.data);
+            let person = res.data
 
-        // đưa person.id vào trong button cập nhật
-        // khi làm thế này thì cái button cập nhật của mình sẽ thêm vào cái data-id có id bằng cái id của food mình ấn sửa
-        getEle("#btnCapNhat").setAttribute("data-id", person.id);
+            // đưa person.id vào trong button cập nhật
+            // khi làm thế này thì cái button cập nhật của mình sẽ thêm vào cái data-id có id bằng cái id của food mình ấn sửa
+            getEle("#btnCapNhat").setAttribute("data-id", person.id);
 
-        getEle("#personID").value = person.id;
-        getEle("#personName").value = person.namePerson;
-        getEle("#personAddress").value = person.address;
-        getEle("#personEmail").value = person.email;
-        getEle("#personType").value = person.type;
-        xetLoaiDoiTuong();
-        if ( person.type === "Student") {
-            getEle("#studentDiemToan").value = person.diemToan;
-            getEle("#studentDiemLy").value = person.diemLy;
-            getEle("#studentDiemHoa").value = person.diemHoa;
-        }
-        else if ( person.type === "Employee" ) {
-            getEle("#employeeDay").value = person.day;
-            getEle("#employeeSalary").value = person.salary;
-        }
-        else if ( person.type === "Customer" ) {
-            getEle("#customerCongTy").value = person.congTy;
-            getEle("#customerHoaDon").value = person.hoaDon;
-            getEle("#customerDanhGia").value = person.danhGia;
-        }
-        else {
-            // do nothing
-        }
-    })
-    .catch((err) => {
-        console.log("err: ", err);
-    })
-    // luôn luôn chạy dù thành công, thất bại
-    .finally(() =>{
-        console.log("Xong");
-    });
+            getEle("#personID").value = person.id;
+            getEle("#personName").value = person.namePerson;
+            getEle("#personAddress").value = person.address;
+            getEle("#personEmail").value = person.email;
+            getEle("#personType").value = person.type;
+            xetLoaiDoiTuong();
+            if (person.type === "Student") {
+                getEle("#studentDiemToan").value = person.diemToan;
+                getEle("#studentDiemLy").value = person.diemLy;
+                getEle("#studentDiemHoa").value = person.diemHoa;
+            }
+            else if (person.type === "Employee") {
+                getEle("#employeeDay").value = person.day;
+                getEle("#employeeSalary").value = person.salary;
+            }
+            else if (person.type === "Customer") {
+                getEle("#customerCongTy").value = person.congTy;
+                getEle("#customerHoaDon").value = person.hoaDon;
+                getEle("#customerDanhGia").value = person.danhGia;
+            }
+            else {
+                // do nothing
+            }
+        })
+        .catch((err) => {
+            console.log("err: ", err);
+        })
+        // luôn luôn chạy dù thành công, thất bại
+        .finally(() => {
+            console.log("Xong");
+        });
 }
 
 
@@ -315,21 +315,21 @@ const updatePerson = () => {
     const idPer = getEle("#btnCapNhat").getAttribute("data-id")
 
     updatePersonByID(idPer, person)
-    .then((res) => {
-        console.log("res.data", res.data);
-        
-        // call API lấy lại danh sách person mới sau khi xoá thành công
-        fetchPerson();
-        // cập nhât xong thì đóng modal
-        getEle("#btnDong").click();
-    })
-    .catch((err) => {
-        console.log("err: ", err);
-    })
-    // luôn luôn chạy dù thành công, thất bại
-    .finally(() =>{
-        console.log("Xong");
-    });
+        .then((res) => {
+            console.log("res.data", res.data);
+
+            // call API lấy lại danh sách person mới sau khi xoá thành công
+            fetchPerson();
+            // cập nhât xong thì đóng modal
+            getEle("#btnDong").click();
+        })
+        .catch((err) => {
+            console.log("err: ", err);
+        })
+        // luôn luôn chạy dù thành công, thất bại
+        .finally(() => {
+            console.log("Xong");
+        });
 }
 
 
@@ -341,25 +341,90 @@ const updatePerson = () => {
  */
 const arrangePerToOrder = () => {
     getPersonList()
-    // thành công thì in ra sp
-    .then(function (res) {
-        const person = res.data;
-        const name = [];
-        person.forEach((value, index) => {            
-            name.push(value.namePerson);
-        })
-        const result = person.sort((a, b) => a.namePerson.localeCompare(b.namePerson));
-        renderTable(result);
+        // thành công thì in ra sp
+        .then(function (res) {
+            const person = res.data;
+            const result = person.sort((a, b) => a.namePerson.localeCompare(b.namePerson));
+            renderTable(result);
 
-    })
-    // thất bại thì báo lỗi, hoặc in ra thông báo tuỳ mình
-    .catch(function (err) {
-    //   offLoading();
-      console.log("err", err);
-    })
-    // luôn luôn chạy dù thành công, thất bại
-    .finally(() =>{
-        console.log("Xong");
-    });
-    
+        })
+        // thất bại thì báo lỗi, hoặc in ra thông báo tuỳ mình
+        .catch(function (err) {
+            //   offLoading();
+            console.log("err", err);
+        })
+        // luôn luôn chạy dù thành công, thất bại
+        .finally(() => {
+            console.log("Xong");
+        });
+
+}
+
+/*
+* @param {*} filterPerson
+* Chức năng: hàm lọc person
+* Tham số: không
+* Chú ý:
+*/
+const filterPerson = () => {
+    var selectPersonType = getEle("#selectPersonType").value;
+    var filterResult = [];
+    var all = "All";
+
+    getPersonList()
+        // thành công thì in ra person
+        .then(function (res) {
+            var person = res.data;
+            if (selectPersonType === all) {
+                renderTable(person);
+            }
+            else {
+                person.forEach((value, index) => {
+                    if (value.type === selectPersonType) {
+                        filterResult.push(value);
+                    }
+                })
+                renderTable(filterResult);
+            }
+        })
+        // thất bại thì báo lỗi, hoặc in ra thông báo tuỳ mình
+        .catch(function (err) {
+            // offLoading();
+            console.log("err", err);
+        });
+}
+
+
+/*
+* @param {*} findPersonByName
+* Chức năng: hàm tìm kiếm person theo name
+* Tham số: không
+* Chú ý:
+*/
+const findPersonByName = () => {
+    var searchName = getEle("#searchName").value.trim()?.toLowerCase();
+    var tbfilterPerson = document.querySelector("#tbFilterPerson");
+
+    // gọi data từ API
+    getPersonList()
+        .then(function (res) {
+            var person = res.data;
+            // tìm kiếm trong danh sách sp mà có tên sau khi đã chuyển về từ thường mà có gồm từ khoá mà muốn tra
+            var result = person.filter(function (value) {
+                return value.namePerson.toLowerCase().includes(searchName);
+            });
+
+            if (result.length == 0) {
+                tbfilterPerson.classList.remove("d-none");
+                tbfilterPerson.classList.add("d-block");
+            } else {
+                tbfilterPerson.classList.remove("d-block");
+                tbfilterPerson.classList.add("d-none");
+                //render lại kết quả tìm thấy
+                renderTable(result);
+            }
+        })
+        .catch(function (err) {
+            console.log("err", err);
+        });
 }
